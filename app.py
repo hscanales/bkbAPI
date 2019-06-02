1182cec5f6467a7af70900d3477f4c657df3a112
hscanales <00136317@uca.edu.sv>
2019-06-01 23:27:15 -0600

    Linktendo

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+
+user = os.environ.get('USER')
+pw = int(os.environ.get('PW'))
+server = os.environ.get('SV')
+tb1 = os.environ.get("tb1")
+tb2 = os.environ.get("tb2")
+tb3 = os.environ.get("tb3")
+client = MongoClient("mongodb+srv://"+user+":"+pw+"@"+server)
+db = client["tb3"]
+partidos = db["tb1"]
+usuarios = db["tb2"]
+
+app = Flask(__name__)
+
+@app.route('/login')
+def getusers():
+    users = [doc for doc in usuarios.find({}, {"_id": 0})]
+
+    return jsonify(users)
+
+
+@app.route('/<string:user>/partidos')
+def getpartidos(user):
+    bkb = [doc for doc in partidos.find({"_id": user}, {"_id": 0})]
+    return jsonify(bkb)
+
+
+if __name__ == '__main__':
+    app.run()
